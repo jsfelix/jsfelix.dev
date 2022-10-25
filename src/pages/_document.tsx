64598b1397231f0dba3dbb283dbcc94550842244
Jefferson Felix <jefferson@felixtech.dev>
2022-10-25 11:02:52 -0300

    fix: icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,26 @@ export default class MyDocument extends Document {
   render(): ReactElement {
     return (
       <Html lang="pt">
-        <Head />
+        <Head>
+          <link rel="shortcut icon" href="/assets/images/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/assets/images/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/assets/images/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/assets/images/favicon-16x16.png"
+          />
+        </Head>
         <body className="bg-white dark:bg-slate-900 text-slate-800 dark:text-slate-300">
           <div>
             <Main />
